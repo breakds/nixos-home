5332f1e35b449d3b53309405d95b715e07b029d3
Break Yang <breakds@gmail.com>
2022-03-29 12:18:46 -0700

    Add vscode to breakds

--- by-user/breakds/default.nix
+++ by-user/breakds/default.nix
@@ -22,4 +22,13 @@
       enableFlakes = true;
     };
   };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode-fhsWithPackages (ps: with ps; [ zlib ]);
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      yzhang.markdown-all-in-one
+    ];
+  };
 }
