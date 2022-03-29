af15e322c93ac99d1aa048a0e70f5d16ab554834
Break Yang <breakds@gmail.com>
2022-03-29 11:53:24 -0700

    Specialized 2105 for cassandra

--- flake.nix
+++ flake.nix
@@ -3,11 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+
     home-manager.url = "github:nix-community/home-manager/release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+
+    nixpkgs-2105.url = "github:NixOS/nixpkgs/nixos-21.05";    
+    home-manager-2105.url = "github:nix-community/home-manager/release-21.05";
+    home-manager-2105.inputs.nixpkgs.follows = "nixpkgs-2105";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: let
+  outputs = { self, nixpkgs, nixpkgs-2105, home-manager, home-manager-2105, ... }: let
     mkHomeManagerModule = { user, imports }: { config, ... }: {
       imports = [
         (home-manager.nixosModules.home-manager)
@@ -23,8 +29,25 @@
       home-manager.users."${user}" = {
         inherit imports;
       };
+
+    };
+
+    mkHomeManagerModule2105 = { user, imports }: { config, ... }: {
+      imports = [
+        (home-manager-2105.nixosModules.home-manager)
+      ];
+      home-manager = {
+        # This is needed to make sure that home-manager follows the
+        # pkgs/nixpkgs specified in this flake.
+        #
+        # Relevant github issue: https://github.com/divnix/devos/issues/30
+        useGlobalPkgs = true;
+        useUserPackages = true;
+      };
+      home-manager.users."${user}" = {
+        inherit imports;
+      };
     }; in {
-      
       nixosModules.breakds-home = mkHomeManagerModule {
         user = "breakds";
         imports = [ ./by-user/breakds ];
@@ -38,7 +61,7 @@
         ];
       };
       
-      nixosModules.cassandra-home = mkHomeManagerModule {
+      nixosModules.cassandra-home = mkHomeManagerModule2105 {
         user = "cassandra";
         imports = [ ./by-user/cassandra ];
       };
@@ -59,7 +82,7 @@
         ];
       };
       
-      nixosConfigurations.cassandra-vm = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.cassandra-vm = nixpkgs-2105.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./machines/cassandra-vm.nix
