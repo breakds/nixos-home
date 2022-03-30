edafa2030a7e92815f02fe0d45d7782a7f40ca2b
Break Yang <breakds@gmail.com>
2022-03-29 21:17:35 -0700

    Use default vscode for cassandra

--- by-user/cassandra/default.nix
+++ by-user/cassandra/default.nix
@@ -17,7 +17,6 @@
 
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode-fhsWithPackages (ps: with ps; [ zlib ]);
     extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       yzhang.markdown-all-in-one
