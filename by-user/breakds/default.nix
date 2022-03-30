d60ace1766c89e11a95548c815180a23a6af0d7c
Break Yang <breakds@gmail.com>
2022-03-29 20:33:12 -0700

    Use default vscode

--- by-user/breakds/default.nix
+++ by-user/breakds/default.nix
@@ -25,7 +25,6 @@
 
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode-fhsWithPackages (ps: with ps; [ zlib ]);
     extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       yzhang.markdown-all-in-one
