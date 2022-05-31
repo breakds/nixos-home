218e6e8243d22fbb1a79689cd8e9ff436f4b0da0
Break Yang <breakds@gmail.com>
2022-05-30 21:59:25 -0700

    Upgrade to 22.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "A bunch of home-manager modules";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
-    home-manager.url = "github:nix-community/home-manager/release-21.11";
+    home-manager.url = "github:nix-community/home-manager/release-22.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
 
