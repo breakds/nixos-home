6e32c26bf8f785dd84beffaa123de48c6d63e1d1
Break Yang <breakds@gmail.com>
2022-06-05 11:11:57 -0700

    2205 for cassandra

--- flake.nix
+++ flake.nix
@@ -6,14 +6,9 @@
 
     home-manager.url = "github:nix-community/home-manager/release-22.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-
-    nixpkgs-2105.url = "github:NixOS/nixpkgs/nixos-21.05";    
-    home-manager-2105.url = "github:nix-community/home-manager/release-21.05";
-    home-manager-2105.inputs.nixpkgs.follows = "nixpkgs-2105";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-2105, home-manager, home-manager-2105, ... }: let
+  outputs = { self, nixpkgs, home-manager, ... }: let
     mkHomeManagerModule = { user, imports }: { config, ... }: {
       imports = [
         (home-manager.nixosModules.home-manager)
@@ -30,23 +25,6 @@
         inherit imports;
       };
 
-    };
-
-    mkHomeManagerModule2105 = { user, imports }: { config, ... }: {
-      imports = [
-        (home-manager-2105.nixosModules.home-manager)
-      ];
-      home-manager = {
-        # This is needed to make sure that home-manager follows the
-        # pkgs/nixpkgs specified in this flake.
-        #
-        # Relevant github issue: https://github.com/divnix/devos/issues/30
-        useGlobalPkgs = true;
-        useUserPackages = true;
-      };
-      home-manager.users."${user}" = {
-        inherit imports;
-      };
     }; in {
       nixosModules.breakds-home = mkHomeManagerModule {
         user = "breakds";
@@ -61,7 +39,7 @@
         ];
       };
       
-      nixosModules.cassandra-home = mkHomeManagerModule2105 {
+      nixosModules.cassandra-home = mkHomeManagerModule {
         user = "cassandra";
         imports = [ ./by-user/cassandra ];
       };
@@ -82,7 +60,7 @@
         ];
       };
       
-      nixosConfigurations.cassandra-vm = nixpkgs-2105.lib.nixosSystem {
+      nixosConfigurations.cassandra-vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./machines/cassandra-vm.nix
