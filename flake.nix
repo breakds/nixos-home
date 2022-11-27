bfb9732ceab110b64acd384e9def514082e9290f
Break Yang <breakds@gmail.com>
2022-11-26 23:54:00 -0800

    Switch to 22.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "A bunch of home-manager modules";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
-    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
