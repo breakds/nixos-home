7e5652c341dbec8e1702f3dc79cee1853131dc29
Break Yang <breakds@gmail.com>
2023-02-07 20:57:04 -0800

    Add TODO

--- by-user/breakds/sync.nix
+++ by-user/breakds/sync.nix
@@ -1,5 +1,8 @@
 { pkgs, lib, ... }:
 
+# TODO(breakds): The cloneRepo script has the problem that git
+# requries ssh, but ssh might not be present. This can happen when the
+# if condition is true (i.e. usually happen on a clean installation).
 let cloneRepo = { source, path } : lib.hm.dag.entryAfter ["writeBoundary"] ''
       if [[ ! -d "$HOME/${path}" ]]; then
         ${pkgs.git}/bin/git clone ${source} $HOME/${path}
@@ -9,20 +12,20 @@ let cloneRepo = { source, path } : lib.hm.dag.entryAfter ["writeBoundary"] ''
     sync-org = pkgs.callPackage ../../pkgs/sync-org {};
 
 in {
-  # home.activation.initPasswordStore = cloneRepo {
-  #   source = "git@github.com:breakds/PastOre.git";
-  #   path = ".password-store";
-  # };
+  home.activation.initPasswordStore = cloneRepo {
+    source = "git@github.com:breakds/PastOre.git";
+    path = ".password-store";
+  };
 
-  # home.activation.initEmacs = cloneRepo {
-  #   source = "git@github.com:breakds/emacs.d.git";
-  #   path = ".emacs.d";
-  # };
+  home.activation.initEmacs = cloneRepo {
+    source = "git@github.com:breakds/emacs.d.git";
+    path = ".emacs.d";
+  };
 
-  # home.activation.initOrg = cloneRepo {
-  #   source = "git@github.com:breakds/org.git";
-  #   path = "org";
-  # };
+  home.activation.initOrg = cloneRepo {
+    source = "git@github.com:breakds/org.git";
+    path = "org";
+  };
 
   systemd.user = {
     # Install the timer
