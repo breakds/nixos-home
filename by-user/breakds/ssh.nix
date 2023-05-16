fb0ec54eec43a1ce97bd9b2e071022d57513415e
Break Yang <breakds@gmail.com>
2023-05-16 09:50:48 -0700

    Add into bmbj

--- by-user/breakds/ssh.nix
+++ by-user/breakds/ssh.nix
@@ -1,7 +1,14 @@
 { pkgs, ... }:
 
 let mkBMBJ = ip: {
-      hostname = "39.105.219.118";
+      hostname = ip;
+      port = 50022;
+      user = "yiqing.yang";
+    };
+
+    mkIntoBMBJ = ip: {
+      hostname = ip;
+      proxyJump = "limbius";
       port = 50022;
       user = "yiqing.yang";
     };
@@ -134,15 +141,25 @@ in {
       };
 
       "bmbj001" = mkBMBJ "39.105.219.118";
+      "into-bmbj001" = mkIntoBMBJ "39.105.219.118";
       "bmbj002" = mkBMBJ "39.106.85.150";
+      "into-bmbj002" = mkIntoBMBJ "39.106.85.150";
       "bmbj003" = mkBMBJ "39.107.81.216";
+      "into-bmbj003" = mkIntoBMBJ "39.107.81.216";
       "bmbj004" = mkBMBJ "39.105.129.73";
+      "into-bmbj004" = mkIntoBMBJ "39.105.129.73";
       "bmbj005" = mkBMBJ "39.105.183.19";
+      "into-bmbj005" = mkIntoBMBJ "39.105.183.19";
       "bmbj006" = mkBMBJ "39.107.255.19";
+      "into-bmbj006" = mkIntoBMBJ "39.107.255.19";
       "bmbj007" = mkBMBJ "39.105.218.151";
+      "into-bmbj007" = mkIntoBMBJ "39.105.218.151";
       "bmbj008" = mkBMBJ "47.94.21.167";
+      "into-bmbj008" = mkIntoBMBJ "47.94.21.167";
       "bmbj009" = mkBMBJ "59.110.171.241";
+      "into-bmbj009" = mkIntoBMBJ "59.110.171.241";
       "bmbj010" = mkBMBJ "101.201.35.69";
+      "into-bmbj010" = mkIntoBMBJ "101.201.35.69";
     };
   };
 }
