bfcc8c9ee434979b3006b8166af7715b61cfe407
Break Yang <breakds@gmail.com>
2023-06-04 14:55:05 -0700

    Improve git

--- by-user/breakds/git.nix
+++ by-user/breakds/git.nix
@@ -8,10 +8,19 @@ in {
     package = lib.mkDefault pkgs.gitAndTools.gitFull;
     userName = lib.mkDefault "Break Yang";
     userEmail = lib.mkDefault "breakds@gmail.com";
+
+    difftastic = {
+      enable = true;
+    };
+
     signing = {
       key = "breakds@gmail.com";
+      # Disabled because otherwise it will ask me to type the password
+      # for the GPG key every time. Maybe until I figure out the GPG
+      # agent thing.
       signByDefault = false;
     };
+
     extraConfig = {
       pull.rebase = true;
       init.defaultBranch = "master";
