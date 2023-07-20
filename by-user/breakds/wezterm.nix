b4eda72e48170b6d489ba5139ea3f764e9513fd5
Break Yang <breakds@gmail.com>
2023-07-20 14:21:17 -0700

    Updatge theme to DanQing

--- by-user/breakds/wezterm.nix
+++ by-user/breakds/wezterm.nix
@@ -6,10 +6,11 @@
 
     extraConfig = ''
       return {
-        font = wezterm.font("JetBrains Mono"),
+        font = wezterm.font("Fira Code"),
         font_size = ${if config.home.bds.laptopXsession then "8.0" else "10.0"},
-        color_scheme = "Tomorrow Night",
+        color_scheme = "DanQing (base16)",
         hide_tab_bar_if_only_one_tab = true,
+        scrollback_lines = 10000,
       }
     '';
   };
