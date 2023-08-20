eb652ee976b77d4b7ee9b7db96b90044ab10c619
Break Yang <breakds@gmail.com>
2023-08-20 12:34:25 -0700

    Update wezterm key binding

--- by-user/breakds/wezterm.nix
+++ by-user/breakds/wezterm.nix
@@ -32,6 +32,28 @@
         color_scheme = "BreakDrucula",
         hide_tab_bar_if_only_one_tab = true,
         scrollback_lines = 10000,
+        keys = {
+          { key = '1', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment, },
+          { key = '2', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment, },
+          { key = '3', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment, },
+          { key = '4', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment, },
+          { key = '5', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment, },
+          { key = '6', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment, },
+          { key = '7', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment, },
+          { key = '8', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment, },
+          { key = '9', mods = 'SUPER', action = wezterm.action.DisableDefaultAssignment, },
+          { key = '1', mods = 'ALT', action = wezterm.action.ActivateTab(0), },
+          { key = '2', mods = 'ALT', action = wezterm.action.ActivateTab(1), },
+          { key = '3', mods = 'ALT', action = wezterm.action.ActivateTab(2), },
+          { key = '4', mods = 'ALT', action = wezterm.action.ActivateTab(3), },
+          { key = '5', mods = 'ALT', action = wezterm.action.ActivateTab(4), },
+          { key = '6', mods = 'ALT', action = wezterm.action.ActivateTab(5), },
+          { key = '7', mods = 'ALT', action = wezterm.action.ActivateTab(6), },
+          { key = '8', mods = 'ALT', action = wezterm.action.ActivateTab(7), },
+          { key = '9', mods = 'ALT', action = wezterm.action.ActivateTab(8), },
+          { key = 'j', mods = 'ALT', action = wezterm.action.ActivateTabRelative(-1), },
+          { key = ';', mods = 'ALT', action = wezterm.action.ActivateTabRelative(1), },
+        },
       }
     '';
   };
