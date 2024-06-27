4fcd6134c2dfb124765df53dd8c7c813f675150e
Break Yang <breakds@gmail.com>
2024-06-27 10:43:51 -0700

    Add new aliases

--- by-user/breakds/git.nix
+++ by-user/breakds/git.nix
@@ -27,5 +27,10 @@ in {
       advice.addIgnoredFile = false;
       http.version = "HTTP/1.1";
     };
+
+    aliases = {
+      # Copied from Kiran Rao: https://kiranrao.ca/2024/06/21/git-config.html
+      logall = "log --graph --abbrev-commit --decorate --date=short --format=format:'%C(bold cyan)%h%C(reset) %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)%an%C(reset) %C(bold yellow)%d%C(reset)' --branches --remotes --tags";
+    };
   };
 }
