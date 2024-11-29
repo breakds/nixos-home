cc256a75e83cb992eab38fde0f46fced1c124d5b
Break Yang <breakds@gmail.com>
2024-11-29 12:22:40 -0800

    Fix wezterm with a workaround

--- by-user/breakds/wezterm.nix
+++ by-user/breakds/wezterm.nix
@@ -25,11 +25,15 @@
       };      
     };
 
+    # TODO(breakds): There is an upstream bug that prevents wezterm from working
+    # correctly, see https://github.com/NixOS/nixpkgs/issues/336069
+    # Adding front_end = "WebGpu" as a workaround for now.
     extraConfig = ''
       return {
-        -- font = wezterm.font("Fira Code"),
-        -- font_size = ${if config.home.bds.laptopXsession then "13.0" else "12.0"},
-        -- color_scheme = "BreakDrucula",
+        front_end = "WebGpu",
+        font = wezterm.font("Fira Code"),
+        font_size = ${if config.home.bds.laptopXsession then "13.0" else "12.0"},
+        color_scheme = "BreakDrucula",
         hide_tab_bar_if_only_one_tab = true,
         scrollback_lines = 10000,
         keys = {
