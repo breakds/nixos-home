dc6f79e22ccabc3df094e0ebeeb13e091e447772
Break Yang <breakds@gmail.com>
2025-05-29 13:45:02 -0700

    profiles.default.extensions for vscode

--- by-user/cassandra/default.nix
+++ by-user/cassandra/default.nix
@@ -25,7 +25,7 @@
   
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       yzhang.markdown-all-in-one
     ];
