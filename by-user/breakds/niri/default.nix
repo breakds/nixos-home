1f9293623f9b4f98e4b88a9a00eb3ac0e08b042f
Break Yang <breakds@gmail.com>
2026-02-07 14:50:23 -0800

    Add more bindings for niri

--- by-user/breakds/niri/default.nix
+++ by-user/breakds/niri/default.nix
@@ -1,10 +1,21 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  niri-session-menu = pkgs.writeShellScriptBin "niri-session-menu" ''
+    choice=$(printf "lock\nexit\nreboot\npoweroff" | ${pkgs.fuzzel}/bin/fuzzel --dmenu -p "Session: ")
+    case "$choice" in
+      lock) noctalia-shell ipc call lockScreen lock ;;
+      exit) niri msg action quit ;;
+      reboot) systemctl reboot ;;
+      poweroff) systemctl poweroff ;;
+    esac
+  '';
+in {
   xdg.configFile."niri/config.kdl".source = ./config.kdl;
 
   home.packages = with pkgs; [
     fuzzel
     noctalia-shell
+    niri-session-menu
   ];
 }
