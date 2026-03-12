ec64a52af05a0b00ec71fa117da79b23315e00e0
Break Yang <breakds@gmail.com>
2026-03-12 09:31:34 -0700

    Add back

--- by-user/breakds/starship.nix
+++ by-user/breakds/starship.nix
@@ -3,6 +3,8 @@
 {
   programs.starship = {
     enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
     settings = {
       format = ''
         [](color_orange)$os$username$hostname[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)$c$cpp$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:color_blue bg:color_bg3)$docker_context$conda$pixi[](fg:color_bg3 bg:color_bg1)$time[ ](fg:color_bg1)$line_break$character'';
